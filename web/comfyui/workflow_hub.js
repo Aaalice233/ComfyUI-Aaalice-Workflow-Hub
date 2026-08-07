5cacca7a0f42ff0bb5891e54b670bded22a85467
Luna_K <65744111+Aaalice233@users.noreply.github.com>
2026-08-07 22:55:42 +0800

    chore(release): 发布 1.0.3

--- web/comfyui/workflow_hub.js
+++ web/comfyui/workflow_hub.js
@@ -272,7 +272,7 @@ app.registerExtension({
   ],
   aboutPageBadges: [
     {
-      label: "Aaalice Workflow Hub v1.0.2",
+      label: "Aaalice Workflow Hub v1.0.3",
       url: "https://github.com/Aaalice233/ComfyUI-Aaalice-Workflow-Hub",
       icon: "pi pi-box",
     },
